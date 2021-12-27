aed4d79f145b1ae7831dd9e6e1292f61ce8b2b69
kelinirt <kelipelitabangsa.ac.id>
2021-12-27 08:47:49 +0700

    menambahkan akmal.py

---
+++ akmal.py
@@ -0,0 +1,14 @@
+txt = 'Hello World!'
+txt2 = 'HelloWorld!'
+txt3 = 'HELLO WORLD!'
+txt4 = 'hello world!'
+txt5 = 'Jello World!'
+
+print("txt: ", txt)
+print("txt: ", txt[10])
+print("txt: ", txt[1],  txt[2], txt[3], txt[4])
+print("txt: ",txt2)
+print("txt: ",txt3)
+print("txt: ",txt4)
+print("txt: ",txt5)
+
